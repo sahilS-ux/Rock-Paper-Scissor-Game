9f028b6587661e141779ffd4ea802cde0a634ad9
sahilS-ux <sahilshrmaa2025@gmail.com>
2025-03-03 15:29:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+# Game rock paper scissor
+# 2 user, rock, scissor or paper.
+# 1 player, 2 computer
+# 1-> scissor, 2-> rock, 3-> paper
+# 1 - 1 or 2-2 or 3-3 tie
+# 1 - 3 you lose
+# import random
+
+# computer_value = random.randint(0, 2)
+
+# print("""
+# Press value according to below format:
+#       0 - Scissor
+#       1 - Paper
+#       2 - Rock
+# """)
+
+# user_value = int(input("\nInsert your option\n"))
+
+# if computer_value == 0:
+#     print("Computer choose scissor")
+# elif computer_value == 1:
+#     print("Computer choose paper")
+# else:
+#     print("Computer choose rock")
+
+# if computer_value == user_value:
+#     print("Tie")
+# elif user_value == 0:
+#     if computer_value == 1:
+#         print("You win!")
+#     elif computer_value == 2:
+#         print("You lose!")
+#     else:
+#         print("Wrong value")
+# elif user_value == 1:
+#     if computer_value == 0:
+#         print("You lose!")
+#     elif computer_value == 2:
+#         print("You win!")
+#     else:
+#         print("Wrong input")
+# elif user_value == 2:
+#     if computer_value == 0:
+#         print("You win!")
+#     elif computer_value == 1:
+#         print("You lose!")
+#     else:
+#         print("Wrong input")
+# else:
+#     print("wrong input!")
+
+
+
+
